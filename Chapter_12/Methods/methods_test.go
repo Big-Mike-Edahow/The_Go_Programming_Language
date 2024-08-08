f7d6804d552dec5c9ab5ec2cf9d37a98e6d3072c
Mike Jackson <perigran_falcon@yahoo.com>
2024-08-08 11:12:35 -0600

    Duty calls.

---
+++ Chapter_12/Methods/methods_test.go
@@ -0,0 +1,18 @@
+// methods_test.go
+
+package methods_test
+
+import (
+	"strings"
+	"time"
+
+	"methods"
+)
+
+func ExamplePrintDuration() {
+	methods.Print(time.Hour)
+}
+
+func ExamplePrintReplacer() {
+	methods.Print(new(strings.Replacer))
+}
